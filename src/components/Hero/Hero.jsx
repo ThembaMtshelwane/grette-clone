fd5560c9b6d4c8c10b5a8d44f5c30cc79b952703
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-29 09:38:58 +0200

    Responsive header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,17 +5,21 @@ import { useState } from "react";
 const Hero = () => {
   const [open, setOpen] = useState(false);
   return (
-    <section className="bg-backgroundColour h-[52vh] md:h-[70vh] border-blue-300 border-[10px] ">
+    <section className="bg-backgroundColour h-[52vh] md:h-[70vh] border-blue-300 border-[10px] overflow-x-hidden ">
       <Navbar open={open} setOpen={setOpen} />
       <div
-        className={`flex transform transition-transform duration-300 ease-in-out border-[5px] h-full items-center border-yellow-400 ${
-          open ? "translate-x-64 ml-[35%]" : "translate-x-0 md:border-green-500"
+        className={`flex   transform transition-transform duration-300 ease-in-out border-[5px] h-full items-center border-yellow-400 ${
+          open ? "translate-x-72 ml-[35%]" : "translate-x-0 md:border-green-500"
         }`}
       >
-        <section className=" w-full flex flex-col h-[70%] justify-center border-2 border-red-500 p-4 lg:p-0 md:border-green-500 md:w-[60%] md:h-fit md:mx-auto  md:mt-32">
+        <section
+          className={`w-full flex flex-col h-[70%] justify-center border-2 border-red-500 p-4 lg:p-0 lg:w-[50%] md:border-green-500 md:h-fit md:mx-auto  md:mt-32 ${
+            open ? "md:w-[100%]" : " md:w-[80%]"
+          }  `}
+        >
           <section
             className={`border-2 border-red-500 md:my-auto md:mr-auto h-[60%] md:w-[80%] md:max-w-[350px] ${
-              open ? "md:ml-[15%]" : "md:ml-0"
+              open ? "md:ml-[0%]" : "md:ml-0"
             }  `}
           >
             <ul className="flex flex-col item border-2">
@@ -40,7 +44,11 @@ const Hero = () => {
             </section>
           </section>
         </section>
-        <section className="absolute w-[35%] h-full right-0 ">
+        <section
+          className={`absolute w-[35%] h-full right-0 transition-transform duration-300 ease-in-out ${
+            open ? "translate-x-72" : "translate-x-0"
+          }`}
+        >
           <img className="h-full" src="/1.PNG" alt="" />
         </section>
       </div>
--- src/components/Hero/Navbar/Navbar.jsx
+++ src/components/Hero/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ open, setOpen }) => {
 
   const SideBar = () => (
     <section
-      className="w-full h-screen bg-backgroundColour flex flex-col relative md:w-[100%]"
+      className="overflow-x-hidden w-full h-screen bg-backgroundColour flex flex-col relative md:w-[100%]"
       role="presentation"
     >
       <nav className="flex border-2 items-center px-3 py-4 w-full  md:border-green-500 md:pt-14">
@@ -29,46 +29,52 @@ const Navbar = ({ open, setOpen }) => {
         </div>
       </nav>
 
-      <section className="p-5 flex flex-col my-auto border-2 h-[50%] w-[65%] md:border-red-600 md:w-[75%] md:ml-[5%]">
-        <section className="mb-4">
-          <a href="/">
-            <h3 className="h3-style"> Employees</h3>
-          </a>
-          <a href="/">
-            <h3 className="h3-style"> Expertise</h3>
-          </a>
-          <a href="/">
-            <h3 className="h3-style"> About Grette</h3>
-          </a>
-        </section>
-        <section className="mb-10">
-          <a href="/">
-            <h4 className="h4-style">Doing Business in Norway </h4>
-          </a>
-          <a href="/">
-            <h4 className="h4-style">Sustainability </h4>
-          </a>
-          <a href="/">
-            <h4 className="h4-style"> Career </h4>
-          </a>
-          <a href="/">
-            <h4 className="h4-style">Grette High-Q</h4>
-          </a>
-          <a href="/">
-            <h4 className="h4-style">Standard Terms of Engagement</h4>
-          </a>
-        </section>
-        <section className="flex items-center mt-10">
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-accent  placeholder-black w-[90%] p-2"
-          />
-          <CiSearch className="bg-accent p-1 w-[20px] scale-[2.5]" />
+      <section className="flex flex-col justify-center h-full border-2">
+        <section className="p-5 flex flex-col border-2 h-[90%] w-[65%] md:border-red-600 md:w-[60%] md:ml-[5%]">
+          <section className="mb-4">
+            <a href="/">
+              <h3 className="h3-style"> Employees</h3>
+            </a>
+            <a href="/">
+              <h3 className="h3-style"> Expertise</h3>
+            </a>
+            <a href="/">
+              <h3 className="h3-style"> About Grette</h3>
+            </a>
+          </section>
+          <section className="mb-10">
+            <a href="/">
+              <h4 className="h4-style">Doing Business in Norway </h4>
+            </a>
+            <a href="/">
+              <h4 className="h4-style">Sustainability </h4>
+            </a>
+            <a href="/">
+              <h4 className="h4-style"> Career </h4>
+            </a>
+            <a href="/">
+              <h4 className="h4-style">Grette High-Q</h4>
+            </a>
+            <a href="/">
+              <h4 className="h4-style">Standard Terms of Engagement</h4>
+            </a>
+          </section>
+          <section className="flex items-center mt-10">
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-accent  placeholder-black w-[90%] p-2"
+            />
+            <CiSearch className="bg-accent p-1 w-[20px] scale-[2.5]" />
+          </section>
         </section>
       </section>
 
-      <section className="absolute w-[17%]  h-full right-0 md:w-[35%]">
+      <section
+        className={`absolute w-[17%] h-full right-0 md:w-[35%] transition-transform duration-[1000ms] ease-in-out ${
+          !open ? "translate-x-72" : "translate-x-0"
+        }`}
+      >
         <img className="h-full" src="/2.PNG" alt="" />
       </section>
     </section>
@@ -105,7 +111,7 @@ const Navbar = ({ open, setOpen }) => {
         onClose={toggleDrawer(false)}
         variant="persistent"
         PaperProps={{
-          className: "w-full md:w-[70%]",
+          className: "w-full md:w-[65%]",
         }}
       >
         {SideBar()}
