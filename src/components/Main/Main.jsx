17aa9405ff50c943452968ab26aafb6b41bd4dc4
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-29 14:40:56 +0200

    Main: Final product

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
         {lawyers.map((lawyer, index) => (
           <div key={index} className=" ">
-            <div key={index} className="border-2 border-red-400 h-[100%] ">
+            <div key={index} className=" h-[100%] ">
               <img
                 src={lawyer.src}
                 alt={`Lawyer ${index}`}
@@ -35,7 +35,7 @@ const Main = () => {
           </div>
         ))}
       </div>
-      <div className="h-40 flex justify-center items-center border-2 border-green-700">
+      <div className="h-40 flex justify-center items-center">
         <a href="#" className="underline italic text-lg">
           See all our stuff.
         </a>
